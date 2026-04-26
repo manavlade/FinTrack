2eb22bb4f8ba36c9510e1f9cce5c7595c940bdb8
manavlade <manavlade14690@gmail.com>
2026-04-26 11:07:44 +0530

    added lazy-loaded login route and updated the folder structure to production

---
+++ fintrack-ui/src/app/features/auth/login/login.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+  
+}
---
+++ fintrack-ui/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost:8080/api/employee',
+    BASE_URL: 'http://localhost:4200/graph',
+    Login_URL: "http://localhost:4200/login",
+    signUp_URL: "http://localhost:4200/signup",
+    dashboard_URL: "http://localhost:4200/dashboard"
+};
